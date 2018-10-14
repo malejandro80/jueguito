da6d3e8581698619c96bd4bb0fb4a83297b6aa13
miguel <m.alejandro_80@hotmail.com>
2018-10-14 19:20:56 -0400

    modificacion clase jugador

--- src/jueguito/jugador.java
+++ src/jueguito/jugador.java
@@ -6,9 +6,6 @@
 package jueguito;
 
 import java.awt.Graphics;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.util.ArrayList;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
@@ -17,32 +14,29 @@ import javax.swing.JPanel;
  * @author miguel
  */
 public class jugador extends JPanel {
+    
     private int x,y;
-  
-    private panel_fondo panel1;
-
+    private int dx;
     //imagenes
     private ImageIcon zombie_der = new ImageIcon(getClass().getResource("/resources/zombie.png"));
     private ImageIcon zombie_izq = new ImageIcon(getClass().getResource("/resources/zombie_izq.png"));
-    
+    panel_fondo panel1;
     private boolean sentido_right;
-
     
     
-    public jugador(int x, int y, panel_fondo panel1){
+    
+    public jugador(int x,int y, panel_fondo panel1){
         super();
         this.x = x;
         this.y = y;
         this.panel1 = panel1;
-       
+        
+        panel1.add(this);
           sentido_right = true;
       
           setBounds(x, y, 100,100);
-          setOpaque(false);
-          panel1.add(this);   
-          //this.addKeyListener(new TAdapter());
-    }
-    
+          setOpaque(false);  
+    }   
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
@@ -53,47 +47,7 @@ public class jugador extends JPanel {
         }
         g.drawImage(dir_zombie.getImage(), 0, 0, getWidth(), getHeight(), this);
     }
-  
-    public void keyPressed(KeyEvent e)
- {
- int direccion = e.getKeyCode();
 
- if (direccion == KeyEvent.VK_RIGHT && x <= 1100){
- x += 15;
- sentido_right = true;
- System.out.println("tecla oprimida");
- }
- if (direccion == KeyEvent.VK_LEFT && x >= 2)
- {
- x -= 15;
- sentido_right = false;
- }
-      
- }
-    
-//    private class TAdapter extends KeyAdapter{
-//
-// public void keyReleased(KeyEvent e){
-// this.keyReleased(e);
-// }
-// public void keyPressed(KeyEvent e){
-// int direccion = e.getKeyCode();
-//
-// if (direccion == KeyEvent.VK_RIGHT && x <= 1100){
-// x += 15;
-// sentido_right = true;
-//     System.out.println("tecla oprimida");
-// }
-// if (direccion == KeyEvent.VK_LEFT && x >= 2)
-// {
-// x -= 15;
-// sentido_right = true;
-// }
-// }
-// }
-
-
- 
  public int getX() {
         return x;
     }
@@ -108,6 +62,13 @@ public class jugador extends JPanel {
 
     public void setY(int y) {
         this.y = y;
+    } 
+       public boolean isSentido_right() {
+        return sentido_right;
     }
-    
+
+    public void setSentido_right(boolean sentido_right) {
+        this.sentido_right = sentido_right;
+    }
 }
+
